3dc0e56e966506d811d51edbfb11506832bbb955
LizaMatyukhina <liza.matyukhina2000@gmail.com>
2020-10-07 10:04:52 +0700

    конец все получилось!

--- src/situation/Main.java
+++ src/situation/Main.java
@@ -2,35 +2,39 @@ package situation;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        Thread thread1 = new Thread(new DemoThreadB());
+    public static void main(String[] args) {
+        Object lock = new Object();
+        Runnable task = () -> {
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                }
+                catch (InterruptedException e) {
+                    System.out.println("interrupted");
+                }
+            }
+        };
+
+        Thread thread1 = new Thread(task);
         System.out.println(thread1.getState());
 
         thread1.start();
         System.out.println(thread1.getState());
 
-        Thread thread2=  new Thread(new DemoThreadB());
-        thread2.start();
-
-        Thread.sleep(1000);
-        System.out.println(thread2.getState());
-        System.exit(0);
-
-    }
-
-    System.out.println()
-
-    }
-
-class DemoThreadB implements Runnable {
-    @Override
-    public void run() {
-        commonResource();
-    }
-
-    public static synchronized void commonResource() {
-        while(true) {
-
+        try {
+            Thread.sleep(3000);
         }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(thread1.getState());
+
+        synchronized (lock) {
+            lock.notify();
+        }
+        System.out.println(thread1.getState());
+
+        thread1.stop();
+        System.out.println(thread1.getState());
     }
 }
